fa22111f1a8a3c5baffe1d879dbde28ddbc861c5
Roland Bali <roland.bali@spagettikod.se>
2014-11-27 07:28:11 +0100

    Initial commit

---
+++ pinger.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	env := r.URL.Path[1:]
+	fmt.Fprintf(w, "Hello, my time is %v\n\nValue of environment variable %v is %v", time.Now(), env, os.Getenv(env))
+}
+
+func main() {
+	http.HandleFunc("/", pingHandler)
+	panic(http.ListenAndServe(":8080", nil))
+}
